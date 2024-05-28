9529abe4161def2f5176dfc1ca6dfd1a994c4bfe
HommePretentieux <h05p1tal13r.leon@gmail.com>
2024-05-28 15:39:06 +0200

    initial commit

---
+++ 1.php
@@ -0,0 +1,21 @@
+<?php
+include '../nav.php';
+
+?>
+<br>
+<div id='exercice1' class="card ">
+    <div class="card-body bg-info align-self-center col-10">
+        <h5 class="card-title">insertion d'un tuple</h5>
+        <?php
+        echo "<ul><li>dsn =$dsn</li><li>username =$username</li><li>password=....</ul><br>";
+        try {
+            $requete = "insert into vin values (3001, 'Champagne de Troyes', 1976, 11.45)";
+            $database->exec($requete);
+            echo "tuple inséré =1";
+        } catch (PDOException $e) {
+            echo "tuple inséré=0";
+        }
+        ?>
+    </div>
+</div>
+<br>
